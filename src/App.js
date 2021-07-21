e020f9c5c6d8fe10b0cbfaea9f747986b4f96dfd
Tesalon Felicien <felicien.tesalon@gmail.com>
2021-07-21 02:55:35 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HurricaneCard from './components/HurricaneCard';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <HurricaneCard />
+      </div>
+    )
+  }
+}
+
+export default App
